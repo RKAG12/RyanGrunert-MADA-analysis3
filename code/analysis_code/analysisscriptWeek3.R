ec96f4716661e34f444ec716fe76c2971a0a8e86
Ryan <grunertryan@gmail.com>
2021-11-05 13:50:00 -0400

    Completed Module 11 Assignment
    Finished assignment 11 for MADA

--- code/analysis_code/analysisscriptWeek3.R
+++ code/analysis_code/analysisscriptWeek3.R
@@ -17,18 +17,18 @@ df <- readRDS(data_location)
 
 ###Feature/Variable removal
 #Removing the yes/no variables for Weakness, Cough, and Myalgia
-df <- subset(df, select = -c(CoughYN, WeaknessYN, CoughYN2, MyalgiaYN))
+df2 <- subset(df, select = -c(CoughYN, WeaknessYN, CoughYN2, MyalgiaYN))
 
 #Coding the three ordered factors Weakness, CoughIntensity, Myalgia
- df <- mutate(df, Weakness = factor(Weakness, levels = c("None", "Mild",
+ df2 <- mutate(df2, Weakness = factor(Weakness, levels = c("None", "Mild",
                                                               "Moderate","Severe"),ordered = TRUE))
- df <- mutate(df, CoughIntensity = factor(Weakness, levels = c("None", "Mild",
+ df2 <- mutate(df2, CoughIntensity = factor(CoughIntensity, levels = c("None", "Mild",
                                                              "Moderate","Severe"),ordered = TRUE))
- df <- mutate(df, Myalgia = factor(Weakness, levels = c("None", "Mild",
+ df2 <- mutate(df2, Myalgia = factor(Myalgia, levels = c("None", "Mild",
                                                              "Moderate","Severe"),ordered = TRUE))
    
 #Removing unbalanced binary predictors with <50 entries in one category
-df2 <- subset(df, select = -c(Hearing, Vision))
+df2 <- subset(df2, select = -c(Hearing, Vision))
 
 data_location2 <- here::here("data","processed_data","PrePanalysisdata.rds")
 saveRDS(df2, file = data_location2)
